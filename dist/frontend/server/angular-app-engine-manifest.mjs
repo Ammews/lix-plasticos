e0049d9c6e6b10e625379a79419f663b68c90a0d
Pedro Ammes <pedro.ammes@gmail.com>
2025-08-06 13:57:50 -0300

    dist changes
    dist changes

--- dist/frontend/server/angular-app-engine-manifest.mjs
+++ dist/frontend/server/angular-app-engine-manifest.mjs
@@ -1,6 +1,6 @@
 
 export default {
-  basePath: '/lix-plasticos',
+  basePath: '/',
   supportedLocales: {
   "en-US": ""
 },
--- dist/frontend/server/angular-app-manifest.mjs
+++ dist/frontend/server/angular-app-manifest.mjs
@@ -2,19 +2,19 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: '/lix-plasticos/',
+  baseHref: '',
   locale: undefined,
   routes: [
   {
     "renderMode": 2,
-    "route": "/lix-plasticos"
+    "route": "/"
   }
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 2107, hash: 'c6963a78d7d0e51c158e113c47b70dd4f01af74057f8075a02981534b1feddf5', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 992, hash: '9098bc3ee2a8ebdc714614a1665ee9acee18141d6cae868ab00e68fbfba38a04', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 11525, hash: '1de1932c2b3420df2c572117031c0de06007a3737cd2537aec05ed027f98e051', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 2092, hash: '1e6581ae11f8c016f6cfab98b43315554b74ca3fe2cb156fba7fe71b33bfa019', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 977, hash: '826e973274858eb17f11291bf62b3be52bc505eb2ed3c443ba9d2b1f9a4af964', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 11507, hash: 'a5093b24c664d83920a2580bc83ee7322266d635b8dcfae936f978c4d1d3b5ee', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles.css': {size: 1577, hash: 'LAmdCHy55wI', text: () => import('./assets-chunks/styles_css.mjs').then(m => m.default)}
   },
 };
